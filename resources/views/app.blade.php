3db843ac9bfcc1006153814a994174d83b66b850
Martin Sloan <martin_sloan@ao.uscourts.gov>
2025-06-13 13:40:49 -0400

    Testing hard code of ip and port

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,7 +39,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes(['url' => config('app.url')])
+        @routes(['url' => 'http://10.179.165.230:8002'])
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
